6144a2e0ee7c662aa61310c2c9db780e2f9de309
EswarReddy Abbavaram <abbavarameswar5@gmail.com>
2026-07-08 20:41:20 +0530

    Add recursive factorial and Fibonacci methods
    Implemented recursive methods for calculating factorial and Fibonacci sequences in Java.

---
+++ recursion.java
@@ -0,0 +1,20 @@
+public class Recursion {
+    static int fact(int n){
+        if(n==0)
+            return 1;
+        return n*fact(n-1);
+    }
+    static int fib(int n){ 
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        return fib(n-1)+fib(n-2);
+    }
+    public static void main(String[] args)
+    {
+        int a= fact(n=5);
+        int b=
+        System.out.println("Factorial="+fact(num));
+    }
+}
